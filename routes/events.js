9293c8a7f2f20a93705210d1798ee2b9dd3bd39c
Sruthi-Palle <pallesruthi12@gmail.com>
2025-12-16 10:17:22 +0530

    corrected workflow issue

--- routes/events.js
+++ routes/events.js
@@ -1,19 +1,28 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import db from '../data/database.js';
+// Import the getDb function to access the database instance
+import { getDb } from "../data/database.js";
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-  const allEvents = await db.collection('events').find().toArray();
+router.get("/", async (req, res) => {
+  // Get the database instance
+  const db = getDb();
+  // Find all events in the 'events' collection and convert them to an array
+  const allEvents = await db.collection("events").find().toArray();
   res.json({ events: allEvents });
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  // Get the database instance
+  const db = getDb();
+  // Get event data from the request body
   const eventData = req.body;
-  const result = await db.collection('events').insertOne({...eventData});
+  // Insert the new event data into the 'events' collection
+  const result = await db.collection("events").insertOne({ ...eventData });
+  // Respond with a 201 status code, a success message, and the created event
   res.status(201).json({
-    message: 'Event created.',
+    message: "Event created.",
     event: { ...eventData, id: result.insertedId },
   });
 });
